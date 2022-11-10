61e82d6c44071ba1b6e3ac6ed3e9eda86dc3262a
hachinrayhan <rayhan.hachin@gmail.com>
2022-11-10 20:00:05 +0600

    user signup done

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,13 +1,35 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, updateProfile } from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
-    const authInfo = { user, loading }
+    const updateUserProfile = profile => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = { user, loading, setLoading, createUser, updateUserProfile };
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
