08d21d084b619bc2088f9199a43fca421b203b51
hachinrayhan <rayhan.hachin@gmail.com>
2022-11-10 18:16:09 +0600

    contexts created

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const authInfo = { user, loading }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
